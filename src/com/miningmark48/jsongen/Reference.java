af867dde9713c69f3fa3af54ac72af7bbeb457de
MiningMark48 <miningmark48@gmail.com>
2016-08-29 00:43:20 -0400

    Revert "Initial upload"
    This reverts commit 74e54467c48d9730f5a6d73f0ce69335e25cfb66.

--- src/com/miningmark48/jsongen/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.miningmark48.jsongen;
-
-public class Reference {
-
-    public static String author = "MiningMark48";
-    public static String version = "0.0.1";
-    public static String gameVersion = "1.9.4";
-
-}

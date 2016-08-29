2feb9bc40d831b7d28456ed6b80e90cbf1d2280d
MiningMark48 <miningmark48@gmail.com>
2016-08-29 01:07:00 -0400

    removed more display stuff that wasn't needed

--- src/com/miningmark48/jsongen/generate/GenerateItem.java
+++ src/com/miningmark48/jsongen/generate/GenerateItem.java
@@ -48,18 +48,6 @@ public class GenerateItem {
             item.println("\t\"parent\": \"item/handheld\",");
             item.println("\t\"textures\": {");
             item.println("\t\t\"layer0\": \"" + modId + ":items/" + textureName + "\"");
-            item.println("\t},");
-            item.println("\t\"display\": {");
-            item.println("\t\t\"thirdperson_righthand\": {");
-            item.println("\t\t\t\"rotation\": [ 0, -90, 55 ],");
-            item.println("\t\t\t\"translation\": [ 0, 4.0, 0.5 ],");
-            item.println("\t\t\t\"scale\": [ 0.85, 0.85, 0.85 ]");
-            item.println("\t\t},");
-            item.println("\t\t\"firstperson_righthand\": {");
-            item.println("\t\t\t\"rotation\": [ 0, -90, 25 ],");
-            item.println("\t\t\t\"translation\": [ 1.13, 3.2, 1.13 ],");
-            item.println("\t\t\t\"scale\": [ 0.68, 0.68, 0.68 ]");
-            item.println("\t\t}");
             item.println("\t}");
             item.println("}");
             item.close();

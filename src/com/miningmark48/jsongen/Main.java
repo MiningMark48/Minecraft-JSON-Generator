5d840cc67d892ebd95a1d8d3b834eb1bc1823344
MiningMark48 <miningmark48@gmail.com>
2016-08-29 00:43:40 -0400

    Revert "Revert "Initial upload""
    This reverts commit af867dde9713c69f3fa3af54ac72af7bbeb457de.

---
+++ src/com/miningmark48/jsongen/Main.java
@@ -0,0 +1,16 @@
+package com.miningmark48.jsongen;
+
+import com.miningmark48.jsongen.gui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setSize(1000, 500);
+        mainWindow.setVisible(true);
+        mainWindow.setLocationRelativeTo(null);
+
+    }
+
+}
---
+++ src/com/miningmark48/jsongen/Reference.java
@@ -0,0 +1,9 @@
+package com.miningmark48.jsongen;
+
+public class Reference {
+
+    public static String author = "MiningMark48";
+    public static String version = "0.0.1";
+    public static String gameVersion = "1.9.4";
+
+}

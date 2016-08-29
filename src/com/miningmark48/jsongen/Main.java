5944253b2f9c497bd3c4dd024b8ee92affa691c7
MiningMark48 <miningmark48@gmail.com>
2016-08-29 12:17:23 -0400

    gui changes

--- src/com/miningmark48/jsongen/Main.java
+++ src/com/miningmark48/jsongen/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
 
         MainWindow mainWindow = new MainWindow();
-        mainWindow.setSize(1000, 500);
+        mainWindow.setSize(800, 400);
         mainWindow.setVisible(true);
         mainWindow.setLocationRelativeTo(null);
 

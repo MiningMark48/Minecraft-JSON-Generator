bbfde708528290a6672da70415a02782424ce37b
MiningMark48 <miningmark48@gmail.com>
2017-04-20 15:41:54 -0400

    Update time.

--- src/com/miningmark48/jsongen/Reference.java
+++ src/com/miningmark48/jsongen/Reference.java
@@ -3,7 +3,7 @@ package com.miningmark48.jsongen;
 public class Reference {
 
     public static String author = "MiningMark48";
-    public static String version = "0.0.8";
+    public static String version = "0.0.9";
     public static String gameVersion = "1.9.4";
 
 }

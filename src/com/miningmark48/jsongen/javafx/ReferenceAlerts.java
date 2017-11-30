427b9d560268a742e0521c2736a2de5e0b3313b6
MiningMark48 <miningmark48@gmail.com>
2017-11-29 21:54:14 -0500

    Block generation now working

--- src/com/miningmark48/jsongen/javafx/ReferenceAlerts.java
+++ src/com/miningmark48/jsongen/javafx/ReferenceAlerts.java
@@ -6,11 +6,16 @@ import javafx.scene.control.Alert;
 public class ReferenceAlerts {
 
     public static Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
+    public static Alert generatedAlert = new Alert(Alert.AlertType.INFORMATION);
 
     public static void init() {
         aboutAlert.setTitle("About");
         aboutAlert.setHeaderText("About Minecraft Json Generator");
         aboutAlert.setContentText("Minecraft JSON Generator v" + Reference.version + " by " + Reference.author);
+
+        generatedAlert.setTitle("Generated");
+        generatedAlert.setHeaderText("Generated");
+        generatedAlert.setContentText("JSON was successfully generated.");
     }
 
 }
--- src/com/miningmark48/jsongen/javafx/controllers/BlockController.java
+++ src/com/miningmark48/jsongen/javafx/controllers/BlockController.java
@@ -1,5 +1,6 @@
 package com.miningmark48.jsongen.javafx.controllers;
 
+import com.miningmark48.jsongen.generate.GenerateBlock;
 import com.miningmark48.jsongen.javafx.ReferenceAlerts;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -8,13 +9,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class BlockController {
 
     @FXML private MenuBar menu_bar;
+    @FXML private TextField fieldModId;
+    @FXML private TextField fieldBlockName;
+    @FXML private TextField fieldTextureName;
+    @FXML private TextField fieldExportPath;
 
     @FXML private void handleCloseButtonAction(ActionEvent event){
         Platform.exit();
@@ -22,6 +30,15 @@ public class BlockController {
 
     @FXML private void handleAboutButtonAction(ActionEvent event){
         ReferenceAlerts.aboutAlert.showAndWait();
+    }
+
+    @FXML private void handlePathButtonAction(ActionEvent event) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Choose resource directory");
+        File defaultDirectory = new File("./export");
+        chooser.setInitialDirectory(defaultDirectory);
+        File selectedDirectory = chooser.showDialog(new Stage());
+        if (selectedDirectory != null) fieldExportPath.setText(selectedDirectory.getPath());
     }
 
     @FXML private void handleAdvancedButtonAction(ActionEvent event) throws IOException {
@@ -33,6 +50,14 @@ public class BlockController {
         stage.setTitle("Block Generator - Advanced");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML private void handleGenerateButtonAction(ActionEvent event) {
+        GenerateBlock.genBlock(fieldModId.getText(), fieldBlockName.getText(), fieldTextureName.getText(), fieldExportPath.getText());
+
+        fieldBlockName.clear();
+        fieldTextureName.clear();
+        ReferenceAlerts.generatedAlert.showAndWait();
     }
 
 }

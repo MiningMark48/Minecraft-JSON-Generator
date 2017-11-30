7ef562b6c4efcc7c63931d37e71d6170573b619e
MiningMark48 <miningmark48@gmail.com>
2017-11-29 23:07:28 -0500

    Pressure plates

--- src/com/miningmark48/jsongen/javafx/controllers/AdvancedBlockController.java
+++ src/com/miningmark48/jsongen/javafx/controllers/AdvancedBlockController.java
@@ -1,6 +1,5 @@
 package com.miningmark48.jsongen.javafx.controllers;
 
-import com.miningmark48.jsongen.generate.GenerateBlock;
 import com.miningmark48.jsongen.javafx.ReferenceAlerts;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -10,10 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
 public class AdvancedBlockController {
@@ -39,6 +36,17 @@ public class AdvancedBlockController {
         Scene scene = new Scene(root, 800, 400);
 
         stage.setTitle("Block - Fence Generator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML private void handlePressurePlateButtonAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/blocks_advanced/pressure_plate.fxml"));
+
+        Scene scene = new Scene(root, 800, 400);
+
+        stage.setTitle("Block - Pressure Plate Generator");
         stage.setScene(scene);
         stage.show();
     }

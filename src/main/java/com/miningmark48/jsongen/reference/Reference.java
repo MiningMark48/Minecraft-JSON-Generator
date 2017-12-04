e6a0154dea35fce14ab2d01ed648cce9a3b5d40b
MiningMark48 <miningmark48@gmail.com>
2017-12-04 00:21:01 -0500

    Dialog icons

--- src/main/java/com/miningmark48/jsongen/reference/Reference.java
+++ src/main/java/com/miningmark48/jsongen/reference/Reference.java
@@ -1,14 +1,11 @@
 package com.miningmark48.jsongen.reference;
 
 import com.miningmark48.jsongen.Main;
-import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.net.URL;
 
 public class Reference {
 
@@ -26,10 +23,14 @@ public class Reference {
         aboutAlert.setTitle("About");
         aboutAlert.setHeaderText("About Minecraft Json Generator");
         aboutAlert.setContentText("Minecraft JSON Generator v" + Reference.version + " by " + Reference.author);
+        Stage aboutStage = (Stage) aboutAlert.getDialogPane().getScene().getWindow();
+        aboutStage.getIcons().add(new Image(Reference.iconURL));
 
         generatedAlert.setTitle("Generated");
         generatedAlert.setHeaderText("Generation Successful");
         generatedAlert.setContentText("JSON was successfully generated.");
+        Stage generatedStage = (Stage) generatedAlert.getDialogPane().getScene().getWindow();
+        generatedStage.getIcons().add(new Image(Reference.iconURL));
     }
 
     public static Scene getDefaultScene(Parent root) {
